506ab28867904abe50571c4992ca24036f76a2f5
Dmitry Tsuprunov <dmitri.tsuprunov@gmail.com>
2019-08-15 23:13:53 -0400

    Added extractMax

--- dataStructures/BinaryHeap.js
+++ dataStructures/BinaryHeap.js
@@ -23,4 +23,33 @@ class MaxBinaryHeap {
     }
     return this;
   }
+
+  extractMax() {
+    const max = this.values[0];
+    this.values[0] = this.values.pop();
+    let currentIndex = 0;
+    let left = currentIndex * 2 + 1;
+    let right = currentIndex * 2 + 2;
+    while (
+      this.values[left] > this.values[currentIndex] ||
+      this.values[right] > this.values[currentIndex]
+    ) {
+      if (this.values[left] >= this.values[right]) {
+        [this.values[left], this.values[currentIndex]] = [
+          this.values[currentIndex],
+          this.values[left],
+        ];
+        currentIndex = left;
+      } else {
+        [this.values[right], this.values[currentIndex]] = [
+          this.values[currentIndex],
+          this.values[right],
+        ];
+        currentIndex = right;
+      }
+      left = currentIndex * 2 + 1;
+      right = currentIndex * 2 + 2;
+    }
+    return max;
+  }
 }
